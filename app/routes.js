216822c2c1d1f75016d46e6ec32dc7ed340b15a0
Tom Doughty <thomashdoughty@gmail.com>
2020-05-20 16:04:04 +0100

    Get content from APIs

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,20 @@
-// External dependencies
 const express = require('express');
+const axios = require('axios');
+
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+router.get('/', async (_, res) => {
+  // Request for coronavirus content page for expanded content
+  const { data: { modules } } = await axios('https://api.nhs.uk/conditions/coronavirus-covid-19/?modules=true&url=www.nhs.uk');
+  const { text: expanded } = modules.find(({ name }) => name === 'symptoms');
+  // Request for coronavirus content FAQs for voice content
+  const { data: { mainEntity } } = await axios('https://api.nhs.uk/conditions/coronavirus-covid-19/faqs');
+  const { acceptedAnswer: { text: voice } } = mainEntity.find(({ name }) => name === 'What are the symptoms of coronavirus?');
+
+  return res.render('index.html', {
+    expanded,
+    voice
+  });
+});
 
 module.exports = router;
